ec80295364b92660fd9e7c12857fea624c3e4742
Lucky-coder14 <79779971+Lucky-coder14@users.noreply.github.com>
2022-06-05 10:05:57 +0530

    project Coding completed..

--- app/src/main/java/com/example/bloodbuddy/Email/Util.java
+++ app/src/main/java/com/example/bloodbuddy/Email/Util.java
@@ -2,6 +2,6 @@ package com.example.bloodbuddy.Email;
 
 public class Util {
     public static  final String EMAIL = "luckykumari1407@gmail.com";
-    public static final String PASSWORD = "dlorlomywszlsqpy";
+    public static final String PASSWORD = "pyhropmmqundvstg";
 
 }
